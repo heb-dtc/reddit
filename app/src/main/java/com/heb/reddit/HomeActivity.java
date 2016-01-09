bb02e2453a60d0d94085d6057e3b2d9f384f7995
flo <fnoel@genymobile.com>
2016-01-09 18:20:30 +0100

    Use service client to trigger authentication

--- app/src/main/java/com/heb/reddit/HomeActivity.java
+++ app/src/main/java/com/heb/reddit/HomeActivity.java
@@ -2,12 +2,30 @@ package com.heb.reddit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.heb.reddit.network.AuthenticationService;
+import com.heb.reddit.network.AuthenticationServiceClient;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button authenticateButton;
+    private AuthenticationServiceClient serviceClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        authenticateButton = (Button) findViewById(R.id.authenticate_button);
+
+        serviceClient = new AuthenticationServiceClient();
+        authenticateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                serviceClient.authenticate();
+            }
+        });
     }
 }
